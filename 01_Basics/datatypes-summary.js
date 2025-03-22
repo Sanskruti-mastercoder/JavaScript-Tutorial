6519f1635eba4e21ecc36c75d486329bf7be4ff9
Sanskruti-mastercoder <sanskrutitoley@gmail.com>
2025-03-22 17:27:04 +0000

    stack and heap memory

--- 01_Basics/datatypes-summary.js
+++ 01_Basics/datatypes-summary.js
@@ -34,3 +34,37 @@ const myFunction = function(){
 console.log(typeof loggedIn);
 console.log(typeof null);
 console.log(typeof myFunction);
+
+// *****************************************************************
+
+// Stack memory (primitve) and Heap memory (non primitve)
+
+let myYoutubeName = "Sanskruti"
+
+let anotherName = myYoutubeName
+anotherName = "sans"
+
+console.log(myYoutubeName);
+console.log(anotherName);
+
+let user1 = {
+    email: "sanskruti@gmail.com",
+    upi: "7058237434@ybl"
+}
+
+let user2 = user1
+
+user2.email = "user@gmail.com"
+
+console.log(user1);
+console.log(user2);
+
+
+// ************************ VERY IMPORTANT ****************************
+// In STACK , the original value is not altered and whatever 
+// we do to the value of the variable after declaring once is actually 
+// changed in the copy of the variable and NOT the original variable 
+
+// Whereas in HEAP , the original value IS ALTERED whenever smt is 
+// done after declaring the value , there is NO COPY of the data everything 
+// is done to the original value only.
