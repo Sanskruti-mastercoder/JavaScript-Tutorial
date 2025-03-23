52c703bf741106615f4ba4cc8d08e7e438ee780b
Sanskruti-mastercoder <sanskrutitoley@gmail.com>
2025-03-23 06:34:39 +0000

    nums and math

---
+++ 01_Basics/06_nums_and_math.js
@@ -0,0 +1,26 @@
+const score = 400 
+const balance = new Number(10) // specifically declaring an object that is a number 
+
+// console.log(balance); 
+// console.log(score);
+
+// console.log(balance.toExponential(2));
+// // console.log(balance.toString().length);
+// console.log(balance.toFixed(3));
+
+const num = 23.2006
+// console.log(num.toFixed(3)); // Rounds off num to 'x' decimal places 
+// console.log(num.toPrecision(2)); // Rounds off num to 'x' significant digits 
+
+const cost = 1000000 
+// console.log(cost.toLocaleString('en-IN')); // Inserts comma in num for convenience 
+
+// console.log(Math);
+// console.log(Math.abs(-5)); // Gives absolute value 
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.3));
+
+
+console.log((Math.random()* 10) + 1) ;
+console.log(Math.random());
